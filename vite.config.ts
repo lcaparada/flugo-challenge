4fab9797a51c6fa99db53b56ef205487533b4db7
lcaparada <lcaparada@gmail.com>
2026-02-10 19:46:25 -0300

    refactor: remove Tailwind CSS dependencies and utility; implement CSS reset styles for improved layout consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
@@ -11,7 +10,6 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
